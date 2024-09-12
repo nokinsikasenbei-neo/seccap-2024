c590ab003d7a8f0246951fe233b7a18e9f148a04
rfLENtlr <gandamyuya@gmail.com>
2024-09-12 17:34:42 +0900

    rev :fix incident

--- rev/challenges/incident/src/main.c
+++ rev/challenges/incident/src/main.c
@@ -14,7 +14,6 @@ unsigned char k[] = {0x76,0x69,0x67,0x65,0x6e,0x65,0x72,0x65};
 
 const char table[TABLE_SIZE] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{}_";
 
-// flagの中身はすべて小文字です。
 int get_index(char c) {
     for (int i = 0; i < TABLE_SIZE; i++) {
         if (table[i] == c) {
@@ -40,47 +39,41 @@ void encrypt(char *data, size_t data_len, const char *key, size_t key_len) {
 }
 
 int main() {
-  // IPアドレスの入力
+
   char ipAddress[16];
   printf("Enter IP address: ");
   if (fgets(ipAddress, sizeof(ipAddress), stdin) == NULL) {
     perror("Failed to read IP address");
     return 1;
   }
-  // 改行文字の削除
+
   size_t len = strlen(ipAddress);
   if (len > 0 && ipAddress[len - 1] == '\n') {
     ipAddress[len - 1] = '\0';
   }
 
-  // ファイルの読み込み
   FILE *file = fopen("/tmp/flag", "r");
   if (file == NULL) {
     perror("Failed to open file");
     return 1;
   }
 
-  // ファイル内容の取得
   char buffer[BUFFER_SIZE];
   size_t bytesRead = fread(buffer, 1, BUFFER_SIZE, file);
   fclose(file);
 
-  // RC4キーと暗号化処理の初期化
   const char *key = k;
   size_t keyLen = strlen(key);
 
-  // バッファの内容をRC4で暗号化
   encrypt((unsigned char *)buffer, bytesRead, (const unsigned char *)key,
           keyLen);
 
-  // ソケットの作成
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     perror("Failed to create socket");
     return 1;
   }
 
-  // 送信先のアドレスとポートの設定
   struct sockaddr_in serverAddr;
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(5000);
@@ -89,14 +82,12 @@ int main() {
     return 1;
   }
 
-  // サーバに接続
   if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
       -1) {
     perror("Failed to connect to server");
     return 1;
   }
 
-  // データの送信
   ssize_t bytesSent = send(sockfd, buffer, bytesRead, 0);
   if (bytesSent == -1) {
     perror("Failed to send data");
@@ -105,7 +96,6 @@ int main() {
 
   printf("Data sent successfully\n");
 
-  // ソケットのクローズ
   close(sockfd);
 
   return 0;
