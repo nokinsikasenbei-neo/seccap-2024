9da0d9eea27949ba48e8110ec0e8112403c07c19
rfLENtlr <gandamyuya@gmail.com>
2024-09-23 18:43:16 +0900

    rev: fix solver

--- rev/challenges/2nd_phase/assembly/solve/solve.py
+++ rev/challenges/2nd_phase/assembly/solve/solve.py
@@ -1,5 +1,5 @@
 def rol(n):
-    return ((n << 1) & 255) | (n >> 7)
+    return ((n << 1) & 0xFF) | (n >> 7)
 
 with open("../file/encrypted_flag.txt", "rb") as f:
     read_data = bytearray(f.read())
